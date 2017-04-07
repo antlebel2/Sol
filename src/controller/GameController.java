4338ea5ced908f0b17975c965a102b11a23254f2
Ayodeji Adebayo <aadebayo2@uco.edu>
2017-04-06 21:14:03 -0500

    minor changes

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -406,7 +406,7 @@ public class GameController implements ActionListener, KeyListener, ComponentLis
         SoundController.getInstance().venusBGM();        
         
         //loading the level
-        BufferedImage levelMap = BufferedImageLoader.getInstance().loadImage("/Images/level3_map.png");
+        BufferedImage levelMap = BufferedImageLoader.getInstance().loadImage("/Images/level.png");
         
         loadImageLevel(levelMap);
         
